1a611bb56e04124ab2fca8d10cd3625e27d3d13b
jinhopkg <jinhopkg@gmail.com>
2022-03-28 13:29:00 +0900

    remove: local build script

--- scripts/localbsc.sh
+++
@@ -1,9 +0,0 @@
-make install
-make build
-sudo rm -r ~/.galaxy
-./bin/galaxyd init local
-cp /Users/j/desktop/galaxies-labs/networks/galaxy-1/genesis.json ~/.galaxy/config
-./bin/galaxyd add-genesis-account t1 1000000000000000uglx --keyring-backend os
-./bin/galaxyd gentx t1 500000000000000uglx --chain-id galaxy-1 
-./bin/galaxyd collect-gentxs
-./bin/galaxyd start

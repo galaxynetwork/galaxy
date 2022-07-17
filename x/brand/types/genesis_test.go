dced7e88f82b09bb16ccaf5a745db970b762d2d3
jinhopkg <jinhopkg@gmail.com>
2022-07-17 18:17:42 +0900

    test: Testing brand genesis types

---
+++ x/brand/types/genesis_test.go
@@ -0,0 +1,51 @@
+package types
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesisState(t *testing.T) {
+	tests := []struct {
+		genState   GenesisState
+		expectPass bool
+	}{
+		{
+			genState:   DefaultGenesisState(),
+			expectPass: true,
+		},
+		{
+			genState:   NewGenesisState(Brands{{Id: "brand1"}}, DefaultParams()),
+			expectPass: true,
+		},
+		{
+			genState:   NewGenesisState(Brands{{Id: "brand1"}, {Id: "brand2"}}, DefaultParams()),
+			expectPass: true,
+		},
+		{
+			genState:   NewGenesisState(Brands{{Id: ""}}, DefaultParams()),
+			expectPass: false,
+		},
+		{
+			genState:   NewGenesisState(Brands{{Id: "brand1"}, {Id: ""}}, DefaultParams()),
+			expectPass: false,
+		},
+		{
+			genState:   NewGenesisState(Brands{{Id: "brand1"}, {Id: "brand1"}}, DefaultParams()),
+			expectPass: false,
+		},
+		{
+			genState:   NewGenesisState(Brands{{Id: "brand1_"}, {Id: "brand2"}}, DefaultParams()),
+			expectPass: false,
+		},
+	}
+
+	for i, test := range tests {
+		if test.expectPass {
+			require.NoError(t, ValidateGenesis(test.genState), "test genState index: %d, genState: %s", i, test.genState.String())
+		} else {
+			require.Error(t, ValidateGenesis(test.genState), "test genState index: %d, genState: %s", i, test.genState.String())
+		}
+	}
+}

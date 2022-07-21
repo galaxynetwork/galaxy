3aa76a60c9545d46b12339c8b635d8c2449dd7e0
jinhopkg <jinhopkg@gmail.com>
2022-07-21 14:03:40 +0900

    feat: Update nft module codec

--- x/nft/types/codec.go
+++ x/nft/types/codec.go
@@ -5,14 +5,22 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
 
 // registers the necessary x/nft concrete types
 func RegisterCodec(cdc *codec.LegacyAmino) {
+	cdc.RegisterConcrete(&MsgCreateClass{}, "galaxy/nft/create-class", nil)
+	cdc.RegisterConcrete(&MsgEditClass{}, "galaxy/nft/edit-class", nil)
 }
 
 // RegisterInterfaces registers the x/nft interfaces types with the interface registry
 func RegisterInterfaces(registry types.InterfaceRegistry) {
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgCreateClass{},
+		&MsgEditClass{},
+	)
+	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
 
 var (

58a4e11871e6fc1b56bd1c32ed8ecf0cc04efd79
jinhopkg <jinhopkg@gmail.com>
2022-07-23 09:26:05 +0900

    refactor: Update brand module store key

--- x/brand/types/keys.go
+++ x/brand/types/keys.go
@@ -18,15 +18,20 @@ const (
 )
 
 var (
-	PlaceHolder = []byte{0x01}
-
-	KeyPrefixBrand = []byte{0x01}
-
+	KeyPrefixBrand        = []byte{0x01}
 	KeyPrefixBrandByOwner = []byte{0x02}
+
+	Delimiter   = []byte{0x00}
+	PlaceHolder = []byte{0x01}
 )
 
 func GetPrefixBrandByOwnerKey(owner sdk.AccAddress) []byte {
-	return append(KeyPrefixBrandByOwner, address.MustLengthPrefix(owner)...)
+	ownerLp := address.MustLengthPrefix(owner)
+	key := make([]byte, len(KeyPrefixBrandByOwner)+len(ownerLp)+len(Delimiter))
+	copy(key, KeyPrefixBrandByOwner)
+	copy(key[len(KeyPrefixBrandByOwner):], ownerLp)
+	copy(key[len(KeyPrefixBrandByOwner)+len(ownerLp):], Delimiter)
+	return key
 }
 
 func NewBrandAddress(brandID string) sdk.AccAddress {

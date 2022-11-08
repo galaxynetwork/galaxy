09a33cc6cb5aaaa842621424ff7fce6c9637a212
jinhopkg <jinhopkg@gmail.com>
2022-11-09 00:05:10 +0900

    chore: Update protobuf config, makefile

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -1,33 +1,37 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+#== Requirements ==
+#
+## make sure your `go env GOPATH` is in the `$PATH`
+## Install:
+## + latest buf (v1.0.0-rc11 or later)
+## + protobuf v3
+#
+## All protoc dependencies must be installed not in the module scope
+## currently we must use grpc-gateway v1
+# cd ~
+# go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+# go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+# go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway@v1.16.0
+# go install github.com/cosmos/cosmos-proto/cmd/protoc-gen-go-pulsar@latest
+# go get github.com/regen-network/cosmos-proto@latest # doesn't work in install mode
+# go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos@v0.3.1
 
 set -eo pipefail
 
-# get protoc executions
-go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
-# get cosmos sdk from github
-go get github.com/cosmos/cosmos-sdk 2>/dev/null
-
-# Get the path of the cosmos-sdk repo from go/pkg/mod
-cosmos_sdk_dir=$(go list -f '{{ .Dir }}' -m github.com/cosmos/cosmos-sdk)
-proto_dirs=$(find . -path ./third_party -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+echo "Generating gogo proto code"
+cd proto
+proto_dirs=$(find ./galaxy -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-  # generate protobuf bind
-   protoc \
-  -I "proto" \
-  -I "$cosmos_sdk_dir/third_party/proto" \
-  -I "$cosmos_sdk_dir/proto" \
-  --gocosmos_out=plugins=interfacetype+grpc,\
-Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
-  $(find "${dir}" -name '*.proto')
-
-  # generate grpc gateway
-   protoc \
-  -I "proto" \
-  -I "$cosmos_sdk_dir/third_party/proto" \
-  -I "$cosmos_sdk_dir/proto" \
-  --grpc-gateway_out=logtostderr=true:. \
-  $(find "${dir}" -maxdepth 1 -name '*.proto')
+  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+    if grep "option go_package" $file &> /dev/null ; then
+      buf generate --template buf.gen.gogo.yaml $file
+    fi
+  done
 done
+
+cd ..
+
 
 # move proto files to the right places
 cp -r github.com/galaxynetwork/galaxy/* ./
